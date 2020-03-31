5c363e2df1e9726534d24289b788f06fb9fc7cba
ajessikafernandes <jessika.alves.fernandes@outlook.com>
2020-03-30 22:48:43 -0300

    create endpoints for admins

--- src/main/java/br/com/dbserver/apibanktransactions/controller/AdminController.java
+++ src/main/java/br/com/dbserver/apibanktransactions/controller/AdminController.java
@@ -1,34 +1,46 @@
-//package br.com.dbserver.apibanktransactions.controller;
-//
-//import br.com.dbserver.apibanktransactions.error.NoRegisteredCustomers;
-//import br.com.dbserver.apibanktransactions.model.AccountStatement;
-//import br.com.dbserver.apibanktransactions.model.Client;
-//import br.com.dbserver.apibanktransactions.service.AdminService;
-//import br.com.dbserver.apibanktransactions.service.ClientService;
-//import lombok.AllArgsConstructor;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//import java.util.List;
-//import java.util.Objects;
-//
-//@AllArgsConstructor
-//@RestController
-//@RequestMapping(path = "admin/")
-//public class AdminController {
-//
-//    AdminService adminService;
-//
-//    @GetMapping("clients/list")
-//    public ResponseEntity<?> clientsList() {
-//        List<AccountStatement> clients = adminService.clientList();
-//        if (clients.size() > 0) {
-//            return ResponseEntity.ok(clients);
-//        } else {
-//            return ResponseEntity.badRequest()
-//                    .body(new NoRegisteredCustomers("Não há clientes cadastrados no sistema."));
-//        }
-//    }
-//}
+package br.com.dbserver.apibanktransactions.controller;
+
+import br.com.dbserver.apibanktransactions.exception.BalanceNonexistentException;
+import br.com.dbserver.apibanktransactions.exception.ClientNotFoundException;
+
+import br.com.dbserver.apibanktransactions.model.Client;
+import br.com.dbserver.apibanktransactions.service.AdminService;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping(path = "admin/")
+public class AdminController {
+
+    AdminService adminService;
+
+    @GetMapping("list")
+    public ResponseEntity<?> listOfClientsAndTheirAccounts() {
+        List<Client> clients = adminService.listOfClientsAndTheirAccounts();
+        if (clients.size() > 0) {
+            return ResponseEntity.ok(clients);
+        } else {
+            return ResponseEntity.badRequest()
+                    .body(new ClientNotFoundException("Não há clientes cadastrados no sistema."));
+        }
+    }
+
+    @GetMapping("accounts/balance")
+    public ResponseEntity<?> sumOfAllBalancesOfAllExistingAccounts() {
+        double amount = adminService.sumOfAllBalancesOfAllExistingAccounts();
+        if (amount > 0) {
+            return ResponseEntity.ok(amount);
+        } else {
+            return ResponseEntity.badRequest()
+                    .body(new BalanceNonexistentException("Balance Nonexistent"));
+        }
+    }
+
+}
