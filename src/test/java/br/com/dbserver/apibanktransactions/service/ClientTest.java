47173b2214a11a54df74a426dc6e351f51f0493e
ajessikafernandes <jessika.alves.fernandes@outlook.com>
2020-04-06 10:13:55 -0300

    starting unit tests

--- src/test/java/br/com/dbserver/apibanktransactions/service/ClientTest.java
+++ src/test/java/br/com/dbserver/apibanktransactions/service/ClientTest.java
@@ -1,24 +1,24 @@
 package br.com.dbserver.apibanktransactions.service;
 
+import br.com.dbserver.apibanktransactions.enums.ClientType;
+import br.com.dbserver.apibanktransactions.exception.ClientNotFoundException;
 import br.com.dbserver.apibanktransactions.model.Client;
 import br.com.dbserver.apibanktransactions.repository.ClientRepository;
+
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
-import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import java.util.Optional;
+
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
 
-@RunWith(MockitoJUnitRunner.class)
-@SpringBootTest
+@RunWith(SpringRunner.class)
 public class ClientTest {
 
     @InjectMocks
@@ -28,16 +28,47 @@ public class ClientTest {
     private ClientRepository repository;
 
     @Before
-    public void setup(){
-        MockitoAnnotations.initMocks(this);
+    public void setUp() {
+        Client client = getClient();
+        Mockito.when(repository.findById(client.getId()))
+                .thenReturn(Optional.of(client));
     }
 
-//    @Test
-//    public void findClientById(){
-//        Client client = new Client();
-//        client.setName("Client Test 1");
-//        client.getMail(client.test@)
-//    }
+    @Test
+    public void clientIsPresent(){
+        Client client = getClient();
+        Mockito.when(repository.findById(client.getId())).thenReturn(Optional.of(client));
+        Assert.assertEquals("New Client", client.getName());
+    }
 
+    @Test
+    public void ClientNotEquals(){
+        Client client = getClient();
+        Mockito.when(repository.findById(client.getId())).thenReturn(Optional.of(client));
+        Assert.assertNotEquals("Client", client.getName());
+    }
+
+    @Test(expected = ClientNotFoundException.class)
+    public void clientNotFound(){
+        Mockito.when(repository.findById(any()));
+        Assert.assertNull(Client.class);
+    }
+
+    @Test
+    public void alterDataClient(){
+        Client client = getClient();
+        Mockito.when(repository.findById(client.getId())).thenReturn(Optional.of(client));
+        service.updateClientData(client, client.getId());
+        client.setMail("new.email@teste.com");
+        Assert.assertEquals("new.email@teste.com", client.getMail());
+    }
+
+    private Client getClient(){
+        Client client = new Client();
+        client.setName("New Client");
+        client.setMail("new.client@teste.com");
+        client.setClientType(ClientType.PF);
+        return client;
+    }
 
 }
